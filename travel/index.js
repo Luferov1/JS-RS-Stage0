90537c9dc272d5c6b9827c8c982fd0f39d5aa3cd
Dmitry Luferov <luferov.dmitry@gmail.com>
2022-07-09 17:32:35 +0300

    feat: add fuctionality to menu

--- travel/index.js
+++ travel/index.js
@@ -1,1 +1,21 @@
 console.log('1. +10 \n2. +20 \n3. +48\n4. +12 \n5. +20 \nmark: 100/100');
+
+const container = document.getElementById('container');
+const openButton = document.getElementById('open');
+const menu = document.getElementById('burger-menu');
+const closeButton = document.getElementById('close');
+const links = document.getElementsByClassName('burger-menu-item');
+
+const open = () => {
+    menu.classList.toggle('burger-menu-open');
+}
+
+const close = () => {
+    menu.classList.toggle('burger-menu-open');
+} 
+
+openButton.addEventListener('click', open);
+closeButton.addEventListener('click', close);
+for (let link of links) {
+    link.addEventListener('click', close);
+}
