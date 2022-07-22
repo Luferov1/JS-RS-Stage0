0b58ac7faa6918961027fd50a899738e78a4daaf
Dmitry Luferov <luferov.dmitry@gmail.com>
2022-07-22 19:58:28 +0300

    feat: add desktop slider

--- travel/index.js
+++ travel/index.js
@@ -21,6 +21,7 @@ const popup = document.querySelector('.popup');
 const popupBackground = document.querySelector('.popup-background');
 const submitPopup = document.querySelector('.form-button');
 const accountButton = document.querySelector('.account');
+const slider = document.querySelector('.destinations-slider');
 
 const openBurger = () => {
     menu.classList.toggle('burger-menu-open');
@@ -94,5 +95,88 @@ submitPopup.addEventListener('click', () => {
 })
 accountButton.addEventListener('click', openPopup);
 
+slider.addEventListener('click', (event) => {
+ 
+    const pageWidth = container.offsetWidth;
 
+    if (event.target === document.querySelector('.destinations-slider') ) return;
+    const target = event.target.closest('.destinations-slider-container');
+    const sliderItems = document.querySelectorAll('.destinations-slider-container');
+    const sliderItemWidth = target.clientWidth;
+    const radios = document.querySelector('.destinations-radio').children;
+    let slide = 0;
+    let index = 0;
+    if (pageWidth > 390) {
+        for (let item of sliderItems) {
+            item.id = `${index}`;
+            index++;
+        }
+
+        if (target.id === '3') {
+            const replacedItem = document.getElementById('0');
+            slider.append(replacedItem);
+        }
+
+        if (target.id === '1') {
+            const replacedItem = document.getElementById('5');
+            slider.prepend(replacedItem);
+        }
+
+        console.log(slider.firstElementChild);
+        if (slider.firstElementChild.classList.contains('usa')) {
+            for (let item of radios) {
+                item.classList.remove('destinations-radio-current-on');
+                item.classList.add('destinations-radio-current-off');
+            }   
+                const radioItem = document.querySelector('.radio2');
+                radioItem.classList.remove('destinations-radio-current-off');
+                radioItem.classList.add('destinations-radio-current-on');
+        }
+
+        if (slider.firstElementChild.classList.contains('spain')) {
+            for (let item of radios) {
+                item.classList.remove('destinations-radio-current-on');
+                item.classList.add('destinations-radio-current-off');
+            }   
+                const radioItem = document.querySelector('.radio3');
+                radioItem.classList.remove('destinations-radio-current-off');
+                radioItem.classList.add('destinations-radio-current-on');
+        }
+
+        if (slider.firstElementChild.classList.contains('japan')) {
+            for (let item of radios) {
+                item.classList.remove('destinations-radio-current-on');
+                item.classList.add('destinations-radio-current-off');
+            }   
+                const radioItem = document.querySelector('.radio1');
+                radioItem.classList.remove('destinations-radio-current-off');
+                radioItem.classList.add('destinations-radio-current-on');
+        }
+    }
+   
+})
+
+
+
+
+
+   // console.log(event.target.getBoundingClientRect().height === 800);
+
+
+    // const sliderWidth = target.closest('.destinations-slider').clientWidth;
+    // const padding = (sliderWidth - sliderItemWidth * 5 ) / 4;
+    // let item = target.getBoundingClientRect();
+    // const itemCoords = {
+    //     top: item.top + window.pageYOffset,
+    //     right: item.right + window.pageXOffset,
+    //     bottom: item.bottom + window.pageYOffset,
+    //     left: item.left + window.pageXOffset
+    // };
+    
+    // Math.max(
+    //     document.body.scrollWidth, document.documentElement.scrollWidth,
+    //     document.body.offsetWidth, document.documentElement.offsetWidth,
+    //     document.body.clientWidth, document.documentElement.clientWidth
+    //   ) ;
+        // const partOfItemOnPage = (pageWidth - padding * 2 - sliderItemWidth) / 2;
 console.log('1. +48 \n2. +15 \n3. +20\nmark: 75/75');
