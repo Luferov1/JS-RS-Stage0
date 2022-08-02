fca71f95bc58312c9ef2bb1e3f9a95e570b10ed5
Dmitry Luferov <luferov.dmitry@gmail.com>
2022-08-02 22:19:56 +0300

    feat: add greetings

--- momentum/js/dataAndTime.js
+++ momentum/js/dataAndTime.js
@@ -1,3 +1,5 @@
+import { showGreeting } from "./greetings.js";
+
 const time = document.querySelector('.time');
 const dateNow = document.querySelector('.date');
 
@@ -7,13 +9,14 @@ const showTime = () => {
     const currentTime = date.toLocaleTimeString();
     time.textContent = currentTime;
     showDate();
+    showGreeting();
     setTimeout(showTime, 1000);
 }
 
 const showDate = () => {
     const date = new Date();
     const options = {weekday: 'long', month: 'long', day: 'numeric', timeZone: 'UTC'};
-    const currentDate = date.toLocaleDateString('be-By', options);
+    const currentDate = date.toLocaleDateString('en-Gb', options);
     dateNow.textContent = currentDate;
 }
 
---
+++ momentum/js/greetings.js
@@ -0,0 +1,30 @@
+const greeting = document.querySelector('.greeting');
+const name = document.querySelector('.name');
+
+
+export const showGreeting = () => {
+    const date = new Date();
+    const hours = date.getHours();
+    const greetingText = getTimeOfTheDay(hours);
+    greeting.textContent = `Good ${greetingText}`
+}
+
+export const getTimeOfTheDay = (hours) => {
+    return hours >= 0 && hours < 6 ? 'night' :
+    hours >= 6 && hours < 12 ? 'morning' :
+    hours >= 12 && hours < 18 ? 'afternoon' :
+    'evening';
+}
+
+const setLocalStorage = () => {
+    localStorage.setItem('name', name.value);
+}
+
+const getLocalStorage = () => {
+    if(localStorage.getItem('name')) {
+        name.value = localStorage.getItem('name');
+    }
+}
+
+window.addEventListener('beforeunload', setLocalStorage)
+window.addEventListener('load', getLocalStorage)
