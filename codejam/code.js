8338c1fe074f70555f12b5526f7b4c0ab22999c4
Dmitry Luferov <luferov.dmitry@gmail.com>
2022-08-23 23:38:58 +0300

    feat: add final html blocks

--- codejam/code.js
+++ codejam/code.js
@@ -1,6 +1,8 @@
 const startGameButton = document.querySelector('.start-button');
 const ancientsContainer = document.querySelector('.choose-container');
 const resetButton = document.querySelector('.reset-button');
+const diffContainer = document.querySelector('.diff-container');
+const gameContainer = document.querySelector('.game-container');
 
 const startGame = () => {
     startGameButton.classList.add('hidden');
@@ -11,6 +13,8 @@ const startGame = () => {
 const resetAll = () => {
     startGameButton.classList.remove('hidden');
     ancientsContainer.classList.add('hidden');
+    diffContainer.classList.add('hidden');
+    gameContainer.classList.add('hidden');
     resetButton.classList.add('hidden');
 }
 
@@ -18,9 +22,19 @@ const chooseAncient = (event) => {
     if (!event.target.classList.contains('ancient')) {
         return
     }
-    
+    ancientsContainer.classList.add('hidden');
+    diffContainer.classList.remove('hidden');
+}
+
+const shuffleDeck = (event) => {
+    if (!event.target.classList.contains('diff-button')) {
+        return
+    }
+    diffContainer.classList.add('hidden');
+    gameContainer.classList.remove('hidden');
 }
 
 startGameButton.addEventListener('click', startGame);
 resetButton.addEventListener('click', resetAll);
-ancientsContainer.addEventListener('click', chooseAncient)
+ancientsContainer.addEventListener('click', chooseAncient);
+diffContainer.addEventListener('click', shuffleDeck)
